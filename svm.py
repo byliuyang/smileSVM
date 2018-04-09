e323fc490d0fa8b51671a16d810c4fea294b70a5
Harry Liu <byliuyang11@gmail.com>
2018-04-09 13:44:13 -0400

    Clean up

--- svm.py
+++ svm.py
@@ -2,7 +2,7 @@ from cvxopt import solvers, matrix
 import numpy as np
 import sklearn.svm
 
-class SVM ():
+class SVM():
     def __init__ (self):
         pass
 
@@ -12,35 +12,37 @@ class SVM ():
     def fit (self, X, y):
         # TODO change these -- they should be matrices or vectors
         n = X.shape[0]
+        dimensions = X.shape[1]
+
         G = make_G(X, y)
-        P = [
-            [1.0, 0.0],
-            [0.0, 0.0]
-        ]
+        I = np.eye(dimensions)
 
-        q = 0
-        h = -1 * np.ones((n,))
+        P = np.zeros((dimensions + 1, dimensions + 1))
 
-        # print(G.shape)
-        # print(P.shape)
-        # print(h.shape)
+        P[0:dimensions, 0:dimensions] = I[0:dimensions, 0:dimensions]
 
+        q = np.zeros((dimensions + 1, ))
+
+        h = -1 * np.ones((n, ))
         # Solve -- if the variables above are defined correctly, you can call this as-is:
+        
         sol = solvers.qp(matrix(P, tc='d'), matrix(q, tc='d'), matrix(G, tc='d'), matrix(h, tc='d'))
 
+        x = sol['x']
+
         # Fetch the learned hyperplane and bias parameters out of sol['x']
-        self.w = 0  # TODO change this
-        self.b = 0  # TODO change this
+        self.w = x[:-1]  # TODO change this
+        self.b = x[-1]  # TODO change this
 
     # Given a 2-D matrix of examples X, output a vector of predicted class labels
-    def predict (self, x):
-        return 0  # TODO fix
+    def predict(self, x):
+        return np.sign(x.dot(self.w) + self.b).reshape((x.shape[0], ))
 
 def make_row(h, size):
     return 
 
 def make_G(X, y):
-    Y = np.array([y, y]).T
+    Y = np.repeat(np.vstack(y), X.shape[1], axis=1)
     return np.hstack((-Y * X, np.reshape(-y, (y.shape[0], 1))))
 
 def test1 ():
@@ -54,11 +56,11 @@ def test1 ():
     print(svm.w, svm.b)
 
     # Compare with sklearn
-    svm = sklearn.svm.SVC(kernel='linear', C=1e15)  # 1e15 -- approximate hard-margin
-    svm.fit(X, y)
-    print(svm.coef_, svm.intercept_)
+    skLearnSvm = sklearn.svm.SVC(kernel='linear', C=1e15)  # 1e15 -- approximate hard-margin
+    skLearnSvm.fit(X, y)
+    print(skLearnSvm.coef_, skLearnSvm.intercept_)
 
-    acc = np.mean(svm.predict(X) == svm.predict(X))
+    acc = np.mean(svm.predict(X) == skLearnSvm.predict(X))
     print("Acc={}".format(acc))
 
 def test2 ():
@@ -77,12 +79,12 @@ def test2 ():
     svm.fit(X, y)
 
     # Compare with sklearn
-    svm = sklearn.svm.SVC(kernel='linear', C=1e15)  # 1e15 -- approximate hard margin
-    svm.fit(X, y)
-    diff = np.linalg.norm(svm.coef_ - svm.w) + np.abs(svm.intercept_ - svm.b)
+    skLearnSvm = sklearn.svm.SVC(kernel='linear', C=1e15)  # 1e15 -- approximate hard margin
+    skLearnSvm.fit(X, y)
+    diff = np.linalg.norm(skLearnSvm.coef_ - svm.w) + np.abs(skLearnSvm.intercept_ - svm.b)
     print(diff)
 
-    acc = np.mean(svm.predict(X) == svm.predict(X))
+    acc = np.mean(svm.predict(X) == skLearnSvm.predict(X))
     print("Acc={}".format(acc))
 
     if acc == 1 and diff < 1e-1:
@@ -90,4 +92,4 @@ def test2 ():
 
 if __name__ == "__main__": 
     test1()
-    # test2()
+    test2()
